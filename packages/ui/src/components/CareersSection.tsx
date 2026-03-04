dc1ff5bbd20975b636de5adcb94e275f44692c4e
Keith Blanco <your_real_email_here@example.com>
2026-03-03 21:55:37 -0700

    add Both option to license dropdown on careers form

--- packages/ui/src/components/CareersSection.tsx
+++ packages/ui/src/components/CareersSection.tsx
@@ -114,6 +114,7 @@ export function CareersSection() {
                       <option value="">Select...</option>
                       <option value="yes-cosmetologist">Yes, Licensed Cosmetologist</option>
                       <option value="yes-barber">Yes, Licensed Barber</option>
+                      <option value="yes-both">Yes, Both</option>
                       <option value="no">Not yet licensed</option>
                     </select>
                   </div>
